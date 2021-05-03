e3674dc377b1b3c0fea3db8b050fd845e67dcf58
weili <liwei72439936@gmail.com>
2021-05-03 16:01:38 +0800

    to assignment semantics

--- jlox/com/craftinginterpreters/lox/Interpreter.java
+++ jlox/com/craftinginterpreters/lox/Interpreter.java
@@ -1,8 +1,13 @@
 package com.craftinginterpreters.lox;
 
+import java.util.List;
+
 import static com.craftinginterpreters.lox.TokenType.*;
 
-public class Interpreter implements Expr.Visitor<Object> {
+public class Interpreter implements Expr.Visitor<Object>,
+        Stmt.Visitor<Void> {
+    private Environment environment = new Environment();
+
     void interpret(Expr expression) {
         try {
             Object value = evaluate(expression);
@@ -10,6 +15,20 @@ public class Interpreter implements Expr.Visitor<Object> {
         } catch (RuntimeError error) {
             Lox.runtimeError(error);
         }
+    }
+
+    void interpret(List<Stmt> statements) {
+        try {
+            for (Stmt statement : statements) {
+                execute(statement);
+            }
+        } catch (RuntimeError error) {
+            Lox.runtimeError(error);
+        }
+    }
+
+    private void execute(Stmt stmt) {
+        stmt.accept(this);
     }
 
     private Object evaluate(Expr expr) {
@@ -95,6 +114,11 @@ public class Interpreter implements Expr.Visitor<Object> {
         return null;
     }
 
+    @Override
+    public Object visitVariableExpr(Expr.Variable expr) {
+        return environment.get(expr.name);
+    }
+
     private void checkNumberOperand(Token operator, Object operand) {
         if (operand instanceof Double) return;
         throw new RuntimeError(operator, "Operand must be a number.");
@@ -129,5 +153,28 @@ public class Interpreter implements Expr.Visitor<Object> {
         }
 
         return object.toString();
+    }
+
+    @Override
+    public Void visitExpressionStmt(Stmt.Expression stmt) {
+        evaluate(stmt.expression);
+        return null;
+    }
+
+    @Override
+    public Void visitPrintStmt(Stmt.Print stmt) {
+        Object value = evaluate(stmt.expression);
+        System.out.println(stringify(value));
+        return null;
+    }
+
+    @Override
+    public Void visitVarStmt(Stmt.Var stmt) {
+        Object value = null;
+        if (stmt.initializer != null) {
+            value = evaluate(stmt.initializer);
+        }
+        environment.define(stmt.name.lexeme, value);
+        return null;
     }
 }
--- jlox/com/craftinginterpreters/tool/GenerateAst.java
+++ jlox/com/craftinginterpreters/tool/GenerateAst.java
@@ -15,16 +15,23 @@ public class GenerateAst {
         }
         String outputDir = args[0];
         defineAst(outputDir, "Expr", Arrays.asList(
+                "Assign      : Token name, Expr value",
                 "Binary      : Expr left, Token operator, Expr right",
                 "Grouping    : Expr expression",
                 "Literal     : Object value",
-                "Unary       : Token operator, Expr right"
+                "Unary       : Token operator, Expr right",
+                "Variable    : Token name"
+        ));
+        defineAst(outputDir, "Stmt", Arrays.asList(
+                "Expression : Expr expression",
+                "Print      : Expr expression",
+                "Var        : Token name, Expr initializer"
         ));
     }
 
     private static void defineAst(String outputDir, String baseName, List<String> types)
             throws IOException {
-       String path = outputDir + "/" + baseName + ".java";
+        String path = outputDir + "/" + baseName + ".java";
         PrintWriter writer = new PrintWriter(path, StandardCharsets.UTF_8);
         writer.println("package com.craftinginterpreters.lox;");
         writer.println();
@@ -53,8 +60,8 @@ public class GenerateAst {
         writer.println("    interface Visitor<R> {");
 
         for (String type : types) {
-           String typeName = type.split(":") [0].trim();
-           writer.println("        R visit" + typeName + baseName + "(" + typeName + " " + baseName.toLowerCase() + ");");
+            String typeName = type.split(":")[0].trim();
+            writer.println("        R visit" + typeName + baseName + "(" + typeName + " " + baseName.toLowerCase() + ");");
         }
 
         writer.println("}");
